4dc96e00e036af402ff5e8a26360d2347c2c356d
Jared <jared.watson@bunn.com>
2022-11-28 14:29:40 -0600

    create dom and add delete functionality

--- script.js
+++ script.js
@@ -34,6 +34,38 @@ function validate (nameValue, urlValue) {
     return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+    //Remove all bookmark elements
+    bookmarksContainer.textContent = '';
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        const { name, url } = bookmark;
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Close icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`)
+        // Favicon/Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://www.google.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', 'Favicon');
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    })
+}
+
 // Fetch bookmarks from LocalStorage
 function fetchBookmarks() {
     // get bookmarks from localStorage if available
@@ -49,7 +81,19 @@ function fetchBookmarks() {
         ];
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
-    console.log(bookmarks);
+    buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+    bookmarks.forEach((bookmark, i) => {
+        if (bookmark.url === url) {
+            bookmarks.splice(i, 1);
+        }
+    });
+    // Update bookmarks array in localstorage, re-populate DOM
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
 }
 
 // Handle data from form
